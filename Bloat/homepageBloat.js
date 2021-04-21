b63d5ee7bb7f2d1c8426dbdf36ee0ee40d40e8ab
W-Dominic <Dom0_o@protonmail.com>
2021-04-21 12:37:26 -0400

    trying to make cooler buttons

--- Bloat/homepageBloat.js
+++ Bloat/homepageBloat.js
@@ -4,3 +4,18 @@ function highlight(x){
 function normal(x){
 	x.style.boxShadow = "0px 0px 0px white";	
 }
+function invertColor(x){
+	var color = window.getComputedStyle(x).color;
+	alert(color);
+	if (color === "var(--fore)"){ 
+		x.style.color = "var(--back)";
+		x.style.backgroundColor = "var(--fore)";
+	}
+	else if (color === "var(--back)"){	
+		x.style.color = "var(--fore)";
+		x.style.backgroundColor = "var(--back)";
+	}
+	else {
+		alert("Something went wrong");
+	}
+}
