91c1755bda3046b45c8ed858369861d5ba1f695e
W-Dominic <Dom0_o@protonmail.com>
2021-04-21 18:45:58 -0400

    made the buttons cooler

--- Bloat/homepageBloat.js
+++ Bloat/homepageBloat.js
@@ -5,13 +5,15 @@ function normal(x){
 	x.style.boxShadow = "0px 0px 0px white";	
 }
 function invertColor(x){
-	var color = window.getComputedStyle(x).color;
-	alert(color);
-	if (color === "var(--fore)"){ 
+	//since the css was defined in an external file we need to use getComputedStyle() to get the value
+	var color = window.getComputedStyle(x,null).getPropertyValue("color");
+	if (color === "rgb(239, 233, 240)"){ 
+		console.log(color);
 		x.style.color = "var(--back)";
 		x.style.backgroundColor = "var(--fore)";
 	}
-	else if (color === "var(--back)"){	
+	else if (color === "rgb(76, 86, 106)"){	
+		console.log(color);
 		x.style.color = "var(--fore)";
 		x.style.backgroundColor = "var(--back)";
 	}
